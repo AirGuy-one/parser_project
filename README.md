240427541a1d7c2f398033d5ccbc81adc5ade88b
airguy <airguy-one@yandex.ru>
2024-02-03 15:45:41 +0300

    Add inserting data to psql db

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ you need to restart selenium container each time when you want to execute script
 ```sh
 sudo docker-compose restart selenium
 ```
+
--- parser_entities.py
+++ parser_entities.py
@@ -1,38 +1,40 @@
 import time
+import psycopg2
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException, TimeoutException, InvalidSessionIdException
 
 
 class YouTubeParser:
     def __init__(self):
         self.cookies_handled = False
         self.driver = webdriver.Remote(command_executor="http://selenium:4444/wd/hub", options=Options())
+        self.conn = psycopg2.connect(dbname='youtube_parser_db', user='youtube_parser_user',
+                                     password='youtube_parser_password', host='db', port='5432')
+        self.cursor = self.conn.cursor()
 
-    def parse_channel(self, channel_url) -> list:
+    def parse_channel(self, channel_url: str) -> list:
+
         self.driver.get(channel_url)
+        # Delay to allow the page to load
+        time.sleep(5)
+
+        if not self.cookies_handled:
+            reject_button = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//button[span[text()='Reject all']]")))
+            reject_button.click()
+            time.sleep(5)
+            self.cookies_handled = True
 
         try:
-            if not self.cookies_handled:
-                print(f'cookies handled {channel_url}')
-                reject_button = WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, "//button[span[text()='Reject all']]")))
-                print(reject_button.text, f'reject button created {channel_url}')
-                reject_button.click()
-                print(f'reject button clicked {channel_url}')
-                time.sleep(5)
-                self.cookies_handled = True
-
-            channel_username = WebDriverWait(self.driver, 10).until(
+            channel_username = WebDriverWait(self.driver, 30).until(
                 EC.presence_of_element_located((By.ID, "text"))).text
 
-            video_links = WebDriverWait(self.driver, 10).until(
+            video_links = WebDriverWait(self.driver, 30).until(
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a#video-title')))
-
-            print(channel_username, f'channel_username {channel_url}')
-            print(video_links[0], f'first video link here {channel_url}')
 
             video_data = []
             for link in video_links:
@@ -42,16 +44,45 @@ class YouTubeParser:
 
             return video_data
 
-        finally:
-            print(f'shit {channel_url}')
+        except InvalidSessionIdException as iside:
+            print(f'InvalidSessionIdException occurred: {iside}')
+        except TimeoutException as te:
+            print(f'TimeoutException occurred while waiting for elements to load: {te}')
+        except WebDriverException as wde:
+            print(f'WebDriverException occurred: {wde}')
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
 
-    @staticmethod
-    def insert_data_to_db(video_data, conn, cursor):
-        print('insert data to db')
-        # for data in video_data:
-        #    query = f"INSERT INTO videos (video_id, channel_username, video_href) VALUES ('{data[0]}', '{data[1]}', '{data[2]}')"
-        #    cursor.execute(query)
-        # conn.commit()
+    def creating_table(self):
+        query = '''
+                CREATE TABLE IF NOT EXISTS videos (
+                    video_id VARCHAR(255),
+                    channel_username VARCHAR(255),
+                    video_href VARCHAR(255)
+                );
+                '''
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    def insert_data_to_db(self, video_data: list):
+        for data in video_data:
+            query = f'''
+                        INSERT INTO videos (
+                            video_id,
+                            channel_username,
+                            video_href
+                        ) VALUES (
+                            '{data[0]}',
+                            '{data[1]}',
+                            '{data[2]}'
+                        )
+                    '''
+            self.cursor.execute(query)
+        self.conn.commit()
+
+    def close_connections(self):
+        self.cursor.close()
+        self.conn.close()
 
     def cleanup(self):
         self.driver.quit()
